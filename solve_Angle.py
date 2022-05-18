578e119b2e81c162e64fd82632e65d35c873ea70
WFz <70101541+ShiaoZhuang@users.noreply.github.com>
2022-05-17 20:25:58 -0700

    Update solve_Angle.py
    new armor board dimensions & new calibration figures

--- solve_Angle.py
+++ solve_Angle.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy as np
-
+import math
 
 def solve_Angle455(imgPoints):
-    width_size_half = 50.0
-    height_size_half = 32.5
-    fx = 324.714304343673
-    cx = 316.498636446160
-    fy = 324.197628291452
-    cy = 236.288203358557
-    k1, k2, p1, p2, k3 = -0.0561272094560157, 0.0613986698843776, 0.0, 0.0, 0.0
+    width_size_half = 70
+    height_size_half = 62.5
+    fx = 645.455984328821
+    cx = 643.077674664939
+    fy = 644.606305889468
+    cy = 357.730289611374
+    k1, k2, p1, p2, k3 = -0.0557535647706463, 0.0538700601952326, -0.000454149012521474, 0.00119677524381670, 0.0
 
     K = np.array([[fx, 0, cx],
                   [0, fy, cy],
@@ -26,5 +26,5 @@ def solve_Angle455(imgPoints):
     retval,rvec,tvec  = cv2.solvePnP(objPoints, imgPoints, cameraMatrix, distCoeffs)
     # cv2.Rodrigues()
     print (retval, rvec, tvec)
-    print("Yaw: ",tvec[(0,0)]/ tvec[(2,0)] / 2 / 3.1415926535897932 * 360)
-    print("Pitch: ",tvec[(1, 0)] / tvec[(2, 0)] / 2 / 3.1415926535897932 * 360)
+    print("Yaw: ",np.arctan(tvec[(0,0)]/ tvec[(2,0)]) / 2 / 3.1415926535897932 * 360)
+    print("Pitch: ",np.arctan(tvec[(1, 0)] / tvec[(2, 0)]) / 2 / 3.1415926535897932 * 360)
