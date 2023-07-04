eeee74b242044168aafcdf20a4fa9ffba269132e
Jonathan Zhang <56613491+Code-MonkeyZhang@users.noreply.github.com>
2023-07-04 12:17:08 -0400

    Update camera_params.py
    2 hik cams

--- camera_params.py
+++ camera_params.py
@@ -13,7 +13,7 @@ camera_params = {
         'exposure': {'red': 45.0, 'blue': 20.0},
         'capture_res': (1280, 720),
         'depth_source': DepthSource.PNP,
-        'fov': (48.2, 33.3)
+        'fov': (90,65)
     },
     'Intel RealSense D435I': {
         'frame_rate': 30,
@@ -38,13 +38,39 @@ camera_params = {
         'p2': 0.00119677524381670,
         'k3': 0.0
     },
-    'HIK cam': {
-        'frame_rate': 30,
-        'exposure': {'red': 45.0, 'blue': 20.0},
+    'HIK MV-CS016-10UC(A)': {
+        # all of HIK camera's params (and units) are adjustable in the MVS platform
+        'frame_rate': 249,
+        'exposure': {'red': 4000.0, 'blue': 5000.0},
         'capture_res': (1280, 720),
         'depth_source': DepthSource.PNP,
-        'fov': (48.2, 33.3),
-
+        'fov': (34.5,26.2),
+        'cx': 747.49,
+        'cy': 581.4007,
+        'fx': 2125.8,
+        'fy': 2122.2,
+        'k1': -0.0369,
+        'k2': -0.1268,
+        'p1': 0.0,
+        'p2': 0.0,
+        'k3': 0.0
+    },
+    'HIK MV-CS016-10UC(B)': {
+        # all of HIK camera's params (and units) are adjustable in the MVS platform
+        'frame_rate': 249,
+        'exposure': {'red': 4000.0, 'blue': 5000.0},
+        'capture_res': (1280, 720),
+        'depth_source': DepthSource.PNP,
+        'fov': (48.33, 30),
+        'cx': 683.0999,
+        'cy': 562.2924,
+        'fx': 2124.7,
+        'fy': 2119.2,
+        'k1': -0.0685,
+        'k2': -0.0623,
+        'p1': 0.0,
+        'p2': 0.0,
+        'k3': 0.0
     }
 }
 
@@ -63,11 +89,12 @@ for cam_name, param_dict in camera_params.items():
             'k3': 0.0
         })
     param_dict['camera_matrix'] = [[param_dict['fx'], 0, param_dict['cx']],
-                                   [0, param_dict['fy'], param_dict['cy']],
-                                   [0, 0, 1]]
+                                            [0, param_dict['fy'], param_dict['cy']],
+                                            [0, 0, 1]]
 
     param_dict['distort_coeffs'] = [param_dict['k1'], param_dict['k2'],
-                                    param_dict['p1'], param_dict['p2'],
-                                    param_dict['k3']]
+                                             param_dict['p1'], param_dict['p2'],
+                                             param_dict['k3']]
+
     if 'camera_offset' not in param_dict:
         param_dict['camera_offset'] = [0.0, 0.0, 0.0]
