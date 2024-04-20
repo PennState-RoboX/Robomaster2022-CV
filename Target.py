2e3ddf697a8e2f4343b57698da205c719345919c
Idhant Gulati <idhant.gulati@gmail.com>
2024-04-20 14:44:38 -0400

    Commented in Target.py

--- Target.py
+++ Target.py
@@ -1,4 +1,7 @@
 class Target:
+    # This main class is used to store the target information in a dictionary
+    # format of the target_dict is- {"depth": x, "Yaw": y, "Pitch": z, "imgPoints": [[0, 0], [0, 0], [0, 0], [0, 0]]}
+    
     def __init__(self, target_dict):
         self.depth = target_dict.get("depth")
         self.yaw = target_dict.get("Yaw")
@@ -7,14 +10,19 @@ class Target:
 
     @property
     def imgPoints(self):
+        # simply return the image points from the dict
         return self._imgPoints
 
     @imgPoints.setter
     def imgPoints(self, value):
+        # set the image points from the dict
         self._imgPoints = value
 
     @property
     def target_coor(self):
+        # return the target coordinates in the format of [bottemLeft, topLeft,topRight, bottomRight]
+        # sample output: [[0, 0], [0, 0], [0, 0], [0, 0]]
+        #                   bl      tl      tr      br
         return [[int(self.imgPoints[0][0]), int(self.imgPoints[0][1])],
                 [int(self.imgPoints[1][0]), int(self.imgPoints[1][1])],
                 [int(self.imgPoints[2][0]), int(self.imgPoints[2][1])],
