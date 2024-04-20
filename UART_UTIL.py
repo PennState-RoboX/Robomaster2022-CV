8436c450a768fb5a469c4fd4c5ef839fcb60e065
Pranav Karra <pranavkarra001@gmail.com>
2024-04-20 14:42:32 -0400

    Update UART_UTIL.py

--- UART_UTIL.py
+++ UART_UTIL.py
@@ -1,27 +1,47 @@
-import serial
+# This File is used to send data to the IMU and get data from the IMU
+# The IMU is connected to the board via UART
+# The IMU is used to get the Pitch and Yaw angles of the board
+
+import serial # The serial library is used to communicate with the IMU
 import time
 import re
 
 
 # Angles are in Byte Format
-def send_data(ser, hex_Yaw, hex_Pitch, checksum, detect_success: bool):
-    # Convert detect_success from boolean to hex string ('01' for True, '00' for False)
-    hex_detect_success = '01' if detect_success else '00'
-    
-    # Construct the packet
+def send_data(ser, hex_int_Pitch, hex_deci_Pitch, hex_int_Yaw, hex_deci_Yaw, sumAll):
+
+    ## ser is the serial object
+    ## hex_int_Pitch is the integer part of the Pitch angle
+    ## hex_deci_Pitch is the decimal part of the Pitch angle
+    ## hex_int_Yaw is the integer part of the Yaw angle
+    ## hex_deci_Yaw is the decimal part of the Yaw angle
+    ## sumAll is the sum of all the bytes in the packet
+
+    # packet = b'\x0d' #header
+    # packet = packet + angleA
+    # packet = packet + angleB
+    # packet = packet + angleC
+    # packet = packet + b'\x03' #not used
+    # packet = packet + b'\x04'
+    # packet = packet + b'\x00'
+    # packet = packet + b'\x04'
+    # packet = packet + b'\x00'
+    # packet = packet + b'\x04'
+    # packet = packet + b'\x00'
     packet = 'a5'  # Header Byte
-    packet += '5a'
-    packet += '0d'  # Length of Packet, 11 bytes
-    packet += hex_Yaw
-    packet += hex_Pitch
-    packet += checksum
-    packet += hex_detect_success
-    packet += 'ff'
-    
-    # Convert the hexadecimal string to bytes
-    packet_bytes = bytes.fromhex(packet)
-    # Write the bytes to the serial port
-    ser.write(packet_bytes)
+    packet = packet + '5a' 
+    packet = packet + '08'  # Length of Packet
+    packet = packet + hex_int_Pitch # Pitch Angle
+    packet = packet + hex_deci_Pitch # Pitch Angle (decimal part)
+    packet = packet + hex_int_Yaw # Yaw Angle
+    packet = packet + hex_deci_Yaw # Yaw Angle (decimal part)
+    # packet = packet + fire_command
+    packet = packet + sumAll
+    packet = packet + 'ff' # End Byte
+    # print(packet)  # Packat before Conversion
+    packet = bytes.fromhex(packet)
+    # print(packet)  # Packet Get Sent
+    ser.write(packet) # Send the packet to the IMU
 
 
 def get_imu(ser):
@@ -30,7 +50,7 @@ def get_imu(ser):
         return imu_value
 
     while (True):
-        # print(ser.in_waiting)
+        print(ser.in_waiting)
         raw_data = ser.read(100)
         data = raw_data.decode('utf-8', 'replace')
         # print(len(raw_data))  # Count bytes
@@ -44,7 +64,6 @@ def get_imu(ser):
                 # Convert the first three elements to floats and return them
                 if len(imu_readings) >= 3:
                     imu_value = [float(x) for x in imu_readings[:3]]
-                    # print(imu_value)
                     return imu_value
             except Exception as e:
                 print(f"Error processing IMU data: {e}")
@@ -59,4 +78,3 @@ if __name__ == '__main__':
     while True:
         imu = get_imu(ser)
         print(imu)
-        # send_data()
