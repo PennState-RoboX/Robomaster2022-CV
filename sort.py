001f34dba88bd7c6fa6b00df7e04097b88701da0
Idhant Gulati <idhant.gulati@gmail.com>
2024-04-20 16:06:37 -0400

    some comments and better indentation.

--- sort.py
+++ sort.py
@@ -34,32 +34,35 @@ np.random.seed(0)
 
 
 def linear_assignment(cost_matrix):
+  # _ is a placeholder for a variable we don't care about / can be ignored
+  # x = row and y = columns for the cost matrix 
   try:
     import lap
     _, x, y = lap.lapjv(cost_matrix, extend_cost=True)
-    return np.array([[y[i],i] for i in x if i >= 0]) #
+    return np.array([[y[i],i] for i in x if i >= 0]) # filter out unassigned detections
   except ImportError:
     from scipy.optimize import linear_sum_assignment
-    x, y = linear_sum_assignment(cost_matrix)
-    return np.array(list(zip(x, y)))
+    x, y = linear_sum_assignment(cost_matrix) 
+    return np.array(list(zip(x, y)))  
 
 
 def iou_batch(bb_test, bb_gt):
   """
   From SORT: Computes IOU between two bboxes in the form [x1,y1,x2,y2]
   """
-  bb_gt = np.expand_dims(bb_gt, 0)
-  bb_test = np.expand_dims(bb_test, 1)
+  bb_gt = np.expand_dims(bb_gt, 0) # ground truth bounding box
+  bb_test = np.expand_dims(bb_test, 1) # predicted bounding box
   
-  xx1 = np.maximum(bb_test[..., 0], bb_gt[..., 0])
-  yy1 = np.maximum(bb_test[..., 1], bb_gt[..., 1])
-  xx2 = np.minimum(bb_test[..., 2], bb_gt[..., 2])
-  yy2 = np.minimum(bb_test[..., 3], bb_gt[..., 3])
-  w = np.maximum(0., xx2 - xx1)
-  h = np.maximum(0., yy2 - yy1)
-  wh = w * h
-  o = wh / ((bb_test[..., 2] - bb_test[..., 0]) * (bb_test[..., 3] - bb_test[..., 1])                                      
-    + (bb_gt[..., 2] - bb_gt[..., 0]) * (bb_gt[..., 3] - bb_gt[..., 1]) - wh)                                              
+  xx1 = np.maximum(bb_test[..., 0], bb_gt[..., 0]) # x1 = max(x1_test, x1_gt)
+  yy1 = np.maximum(bb_test[..., 1], bb_gt[..., 1]) # y1 = max(y1_test, y1_gt)
+  xx2 = np.minimum(bb_test[..., 2], bb_gt[..., 2]) # x2 = min(x2_test, x2_gt)
+  yy2 = np.minimum(bb_test[..., 3], bb_gt[..., 3]) # y2 = min(y2_test, y2_gt)
+  w = np.maximum(0., xx2 - xx1) # width = max(0, x2 - x1)
+  h = np.maximum(0., yy2 - yy1) # height = max(0, y2 - y1)
+  wh = w * h 
+  # o = intersection / union
+  o = wh / ((bb_test[..., 2] - bb_test[..., 0]) * (bb_test[..., 3] - bb_test[..., 1]) 
+            + (bb_gt[..., 2] - bb_gt[..., 0]) * (bb_gt[..., 3] - bb_gt[..., 1]) - wh)                                         
   return(o)  
 
 
@@ -85,10 +88,10 @@ def convert_x_to_bbox(x,score=None):
   """
   w = np.sqrt(x[2] * x[3])
   h = x[2] / w
-  if(score==None):
-    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.]).reshape((1,4))
+  if (score == None):
+    return np.array([x[0] - w/2., x[1] - h/2., x[0] + w/2., x[1] + h/2.]).reshape((1, 4))
   else:
-    return np.array([x[0]-w/2.,x[1]-h/2.,x[0]+w/2.,x[1]+h/2.,score]).reshape((1,5))
+    return np.array([x[0] - w/2., x[1] - h/2., x[0] + w/2., x[1] + h/2., score]).reshape((1, 5))
 
 
 class KalmanBoxTracker(object):
@@ -101,15 +104,27 @@ class KalmanBoxTracker(object):
     Initialises a tracker using initial bounding box.
     """
     #define constant velocity model
-    self.kf = KalmanFilter(dim_x=7, dim_z=4) 
-    self.kf.F = np.array([[1,0,0,0,1,0,0],[0,1,0,0,0,1,0],[0,0,1,0,0,0,1],[0,0,0,1,0,0,0],  [0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]])
-    self.kf.H = np.array([[1,0,0,0,0,0,0],[0,1,0,0,0,0,0],[0,0,1,0,0,0,0],[0,0,0,1,0,0,0]])
+    self.kf = KalmanFilter(dim_x = 7, dim_z = 4) 
 
-    self.kf.R[2:,2:] *= 10.
-    self.kf.P[4:,4:] *= 1000. #give high uncertainty to the unobservable initial velocities
+    self.kf.F = np.array([[1, 0, 0, 0, 1, 0, 0],
+                [0, 1, 0, 0, 0, 1, 0],
+                [0, 0, 1, 0, 0, 0, 1],
+                [0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 0, 1]])
+
+    self.kf.H = np.array([[1, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0]])
+
+
+    self.kf.R[2:, 2:] *= 10.
+    self.kf.P[4:, 4:] *= 1000. #give high uncertainty to the unobservable initial velocities
     self.kf.P *= 10.
-    self.kf.Q[-1,-1] *= 0.01
-    self.kf.Q[4:,4:] *= 0.01
+    self.kf.Q[-1, -1] *= 0.01
+    self.kf.Q[4:, 4:] *= 0.01
 
     self.kf.x[:4] = convert_bbox_to_z(bbox)
     self.time_since_update = 0
@@ -134,11 +149,11 @@ class KalmanBoxTracker(object):
     """
     Advances the state vector and returns the predicted bounding box estimate.
     """
-    if((self.kf.x[6]+self.kf.x[2])<=0):
+    if (self.kf.x[6] + self.kf.x[2]) <= 0:
       self.kf.x[6] *= 0.0
     self.kf.predict()
     self.age += 1
-    if(self.time_since_update>0):
+    if self.time_since_update > 0:
       self.hit_streak = 0
     self.time_since_update += 1
     self.history.append(convert_x_to_bbox(self.kf.x))
@@ -157,27 +172,27 @@ def associate_detections_to_trackers(detections,trackers,iou_threshold = 0.3):
 
   Returns 3 lists of matches, unmatched_detections and unmatched_trackers
   """
-  if(len(trackers)==0):
-    return np.empty((0,2),dtype=int), np.arange(len(detections)), np.empty((0,5),dtype=int)
+  if(len(trackers) == 0):
+    return np.empty((0, 2),dtype=int), np.arange(len(detections)), np.empty((0, 5),dtype = int)
 
   iou_matrix = iou_batch(detections, trackers)
 
   if min(iou_matrix.shape) > 0:
     a = (iou_matrix > iou_threshold).astype(np.int32)
     if a.sum(1).max() == 1 and a.sum(0).max() == 1:
-        matched_indices = np.stack(np.where(a), axis=1)
+        matched_indices = np.stack(np.where(a), axis = 1)
     else:
       matched_indices = linear_assignment(-iou_matrix)
   else:
-    matched_indices = np.empty(shape=(0,2))
+    matched_indices = np.empty(shape=(0, 2))
 
   unmatched_detections = []
   for d, det in enumerate(detections):
-    if(d not in matched_indices[:,0]):
+    if(d not in matched_indices[:, 0]):
       unmatched_detections.append(d)
   unmatched_trackers = []
   for t, trk in enumerate(trackers):
-    if(t not in matched_indices[:,1]):
+    if(t not in matched_indices[:, 1]):
       unmatched_trackers.append(t)
 
   #filter out matched with low IOU
@@ -276,7 +291,8 @@ if __name__ == '__main__':
   total_time = 0.0
   total_frames = 0
   colours = np.random.rand(32, 3) #used only for display
-  if(display):
+  
+  if display:
     if not os.path.exists('mot_benchmark'):
       print('\n\tERROR: mot_benchmark link not found!\n\n    Create a symbolic link to the MOT benchmark\n    (https://motchallenge.net/data/2D_MOT_2015/#download). E.g.:\n\n    $ ln -s /path/to/MOT2015_challenge/2DMOT2015 mot_benchmark\n\n')
       exit()
@@ -286,13 +302,12 @@ if __name__ == '__main__':
 
   if not os.path.exists('output'):
     os.makedirs('output')
+  
   pattern = os.path.join(args.seq_path, phase, '*', 'det', 'det.txt')
+  
   for seq_dets_fn in glob.glob(pattern):
-    mot_tracker = Sort(max_age=args.max_age, 
-                       min_hits=args.min_hits,
-                       iou_threshold=args.iou_threshold) #create instance of the SORT tracker
+    mot_tracker = Sort(max_age=args.max_age, min_hits=args.min_hits, iou_threshold=args.iou_threshold) #create instance of the SORT tracker
     seq_dets = np.loadtxt(seq_dets_fn, delimiter=',')
-    seq = seq_dets_fn[pattern.find('*'):].split(os.path.sep)[0]
     
     with open(os.path.join('output', '%s.txt'%(seq)),'w') as out_file:
       print("Processing %s."%(seq))
@@ -314,10 +329,10 @@ if __name__ == '__main__':
         total_time += cycle_time
 
         for d in trackers:
-          print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1'%(frame,d[4],d[0],d[1],d[2]-d[0],d[3]-d[1]),file=out_file)
+          print('%d, %d, %.2f ,%.2f ,%.2f ,%.2f ,1,-1 ,-1 ,-1'%(frame, d[4], d[0], d[1], d[2]-d[0], d[3]-d[1]), file=out_file)
           if(display):
             d = d.astype(np.int32)
-            ax1.add_patch(patches.Rectangle((d[0],d[1]),d[2]-d[0],d[3]-d[1],fill=False,lw=3,ec=colours[d[4]%32,:]))
+            ax1.add_patch(patches.Rectangle((d[0], d[1]), d[2] - d[0], d[3] - d[1], fill = False, lw = 3, ec = colours[d[4] % 32,:]))
 
         if(display):
           fig.canvas.flush_events()
